af749b110561cf2e430a6946a265ddcfe9c51531
Anh Craft <huynhduyanh123123@gmail.com>
2018-07-19 20:12:32 +0700

    v1.6.6.7: Updated the annotation handler

--- Main/src/java/org/anhcraft/spaciouslib/annotations/AnnotationHandler.java
+++ Main/src/java/org/anhcraft/spaciouslib/annotations/AnnotationHandler.java
@@ -7,6 +7,11 @@ import java.util.List;
 public class AnnotationHandler {
     private static HashMap<Class, List<Object>> data = new HashMap<>();
 
+    /**
+     * Registers the given object and its class.<br>By registering, it allowed SpaciousLib to handle custom annotations. You only need to call this method only one per object.
+     * @param object an object
+     * @param clazz the class of the given object
+     */
     public static void register(Class clazz, Object object){
         List<Object> x = new ArrayList<>();
         if(data.containsKey(clazz)){
@@ -16,6 +21,11 @@ public class AnnotationHandler {
         data.put(clazz, x);
     }
 
+    /**
+     * Unregisters the given registered object and its class.<br>Only calls this method whenever you handled completely and don't want to use any annotations anymore. If there's a working annotation, shouldn't call this method!
+     * @param object an registered object
+     * @param clazz the class of the given object
+     */
     public static void unregister(Class clazz, Object object){
         List<Object> x = new ArrayList<>();
         if(data.containsKey(clazz)){
