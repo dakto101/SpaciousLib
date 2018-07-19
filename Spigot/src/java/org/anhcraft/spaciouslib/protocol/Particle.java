5ca7ac27c017ec71ae3e5576411099e4acfdcf3c
Anh Craft <huynhduyanh123123@gmail.com>
2018-07-19 20:07:26 +0700

    v1.6.6.5: Particle now supports v1.13

--- Spigot/src/java/org/anhcraft/spaciouslib/protocol/Particle.java
+++ Spigot/src/java/org/anhcraft/spaciouslib/protocol/Particle.java
@@ -5,6 +5,7 @@ import org.anhcraft.spaciouslib.utils.Group;
 import org.anhcraft.spaciouslib.utils.ReflectionUtils;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.inventory.ItemStack;
 
 import java.awt.*;
@@ -13,11 +14,24 @@ import java.awt.*;
  * A class helps you to send particle packets
  */
 public class Particle {
-    public static class Dust {
+    public static class Param{
+        private Type particle;
+
+        Param(Type particle) {
+            this.particle = particle;
+        }
+
+        public Type getParticle() {
+            return particle;
+        }
+    }
+
+    public static class DustParam extends Param{
         private Color color;
         private float size;
 
-        public Dust(Color color, float size) {
+        public DustParam(Type particle, Color color, float size) {
+            super(particle);
             this.color = color;
             this.size = size;
         }
@@ -28,6 +42,29 @@ public class Particle {
 
         public float getSize() {
             return size;
+        }
+    }
+
+    public static class ItemParam extends Param {
+        private ItemStack item;
+
+        public ItemParam(Type particle, ItemStack item) {
+            super(particle);
+            this.item = item;
+        }
+
+        public ItemParam(Type particle, Material material) {
+            super(particle);
+            this.item = new ItemStack(material, 1);
+        }
+    }
+
+    public static class BlockDataParam extends Param{
+        private BlockData data;
+
+        public BlockDataParam(Type particle, BlockData data) {
+            super(particle);
+            this.data = data;
         }
     }
 
@@ -48,7 +85,7 @@ public class Particle {
         SMOKE_LARGE("large_smoke"),
         SPELL("effect"),
         SPELL_INSTANT("instant_effect"),
-        SPELL_MOB("entity_effect", Dust.class),
+        SPELL_MOB("entity_effect", DustParam.class),
         SPELL_MOB_AMBIENT("ambient_entity_effect"),
         SPELL_WITCH("witch"),
         DRIP_WATER("dripping_water"),
@@ -64,15 +101,15 @@ public class Particle {
         @Deprecated
         FOOTSTEP(null),
         CLOUD("cloud"),
-        REDSTONE("dust", Dust.class),
+        REDSTONE("dust", DustParam.class),
         SNOWBALL("item_snowball"),
         @Deprecated
         SNOW_SHOVEL(null),
         SLIME("item_slime"),
         HEART("heart"),
         BARRIER("barrier"),
-        ITEM_CRACK("item", ItemStack.class),
-        BLOCK_CRACK("block", Material.class),
+        ITEM_CRACK("item", ItemParam.class),
+        BLOCK_CRACK("block", BlockDataParam.class),
         @Deprecated
         BLOCK_DUST(null),
         WATER_DROP("rain"),
@@ -83,7 +120,7 @@ public class Particle {
         END_ROD("end_rod"),
         DAMAGE_INDICATOR("damage_indicator"),
         SWEEP_ATTACK("sweep_attack"),
-        FALLING_DUST("falling_dust", Material.class),
+        FALLING_DUST("falling_dust", BlockDataParam.class),
         TOTEM("totem_of_undying"),
         SPIT("spit"),
         SQUID_INK("squid_ink"),
@@ -109,63 +146,75 @@ public class Particle {
             return id;
         }
 
-        public Class<?> getDataClass() {
+        public Class<?> getParamClass() {
             return clazz;
         }
     }
 
     public static PacketSender create(Type type, Location location, int count){
-        return create(type, location, count, 0, 0, 0, false, 0, Material.AIR, 0);
+        return create(new Param(type), location, count, 0, 0, 0, false, 0);
     }
 
     public static PacketSender create(Type type, Location location, Color color){
-        return create(type, location, 0, 0, 0, 0, false, 1, new Dust(color, 1));
+        return create(new DustParam(type, color, 1), location, 0, 0, 0, 0, false, 1);
+    }
+
+    public static PacketSender create(Type type, Location location, Color color, int size){
+        return create(new DustParam(type, color, size), location, 0, 0, 0, 0, false, 1);
     }
 
     public static PacketSender create(Color color, Location location){
-        return create(Type.REDSTONE, location, 0, 0, 0, 0, false, 1, new Dust(color, 1));
+        return create(new DustParam(Type.REDSTONE, color, 1), location, 0, 0, 0, 0, false, 1);
     }
 
     public static PacketSender create(Type type, Location location, ItemStack item){
-        return create(type, location, 0, 0, 0, 0, false, 1, item);
+        return create(new ItemParam(type, item), location, 0, 0, 0, 0, false, 1);
     }
 
     public static PacketSender create(ItemStack item, Location location){
-        return create(Type.ITEM_CRACK, location, 0, 0, 0, 0, false, 1, item);
-    }
-
-    public static PacketSender create(Type type, Location location, int count, int speed){
-        return create(type, location, count, 0, 0, 0, false, speed, null);
+        return create(new ItemParam(Type.ITEM_CRACK, item), location, 0, 0, 0, 0, false, 1);
     }
 
     public static PacketSender create(Type type, Location location, int count, Material material,
                                       @Deprecated int data){
-        return create(type, location, count, 0, 0, 0, false, 0, material);
+        return create(new ItemParam(type, material), location, count, 0, 0, 0, false, 0);
     }
 
     public static PacketSender create(Type type, Location location, int count, float offsetX, float offsetY, float offsetZ, boolean longDistance, float speed, Material material, @Deprecated int data){
-        return create(type, location, count, offsetX, offsetY, offsetZ, longDistance, speed, material);
+        return create(new ItemParam(type, material), location, count, offsetX, offsetY, offsetZ,
+                longDistance, speed);
     }
 
-    public static PacketSender create (Type type, Location location, int count, float offsetX, float offsetY, float offsetZ, boolean longDistance, float speed, Object data){
-        if(type.getId() == null){
+    public static PacketSender create (Param param, Location location, int count, float offsetX, float offsetY, float offsetZ, boolean longDistance, float speed){
+        if(param.particle.getId() == null){
             return null;
         }
-        if(type.getDataClass() != null && data.getClass().isAssignableFrom(type.getDataClass())){
-            return null;
+        if(param.particle.getParamClass() != null &&
+                param.getClass().isAssignableFrom(param.particle.getParamClass())){
+            try {
+                throw new Exception(param.particle.toString() + " must have a valid particle param!");
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
         }
         try {
-            Class<?> packetPlayOutWorldParticlesClass = Class.forName("net.minecraft.server."+v+".PacketPlayOutWorldParticles");
             String v = GameVersion.getVersion().toString();
+            Class<?> packetPlayOutWorldParticlesClass = Class.forName("net.minecraft.server."+v+".PacketPlayOutWorldParticles");
             if(GameVersion.is1_13Above()){
                 Class<?> particleClass = Class.forName("net.minecraft.server."+v+".Particle");
                 Class<?> particleParamClass = Class.forName("net.minecraft.server."+v+".ParticleParam");
                 Class<?> registryMaterialClass = Class.forName("net.minecraft.server."+v+".RegistryMaterials");
                 Class<?> minecraftKeyClass = Class.forName("net.minecraft.server."+v+".MinecraftKey");
                 Class<?> particleParamRedstoneClass = Class.forName("net.minecraft.server."+v+".ParticleParamRedstone");
+                Class<?> particleParamItemClass = Class.forName("net.minecraft.server."+v+".ParticleParamItem");
+                Class<?> particleParamBlockClass = Class.forName("net.minecraft.server."+v+".ParticleParamBlock");
+                Class<?> craftItemStackClass = Class.forName("org.bukkit.craftbukkit." + v + ".inventory.CraftItemStack");
+                Class<?> craftBlockDataClass = Class.forName("org.bukkit.craftbukkit." + v + ".block.data.CraftBlockData");
+                Class<?> IBlockData = Class.forName("net.minecraft.server."+v+".IBlockData");
+                Class<?> nmsItemStackClass = Class.forName("net.minecraft.server." + v + ".ItemStack");
                 Object minecraftKey = ReflectionUtils.getConstructor(minecraftKeyClass, new Group<>(
                         new Class<?>[]{String.class},
-                        new Object[]{type.getId()}
+                        new Object[]{param.particle.getId()}
                 ));
                 Object particleRegistry = ReflectionUtils.getStaticField("REGISTRY", particleClass);
                 Object particle = ReflectionUtils.getMethod("get", registryMaterialClass,
@@ -174,17 +223,25 @@ public class Particle {
                         new Object[]{minecraftKey}
                 ));
                 Object particleParam = null;
-                if(type.getDataClass() == null){
+                if(param.particle.getParamClass() == null){
                     particleParam = ReflectionUtils.cast(particleParamClass, particle);
-                } else if(type.getDataClass().equals(ItemStack.class)){
-                    particleParam = ;
-                } else if(type.getDataClass().equals(Material.class)){
-                    particleParam = ;
-                } else if(type.getDataClass().equals(Dust.class)){
-                    Dust dust = (Dust) data;
+                } else if(param instanceof ItemParam){
+                    particleParam = ReflectionUtils.getConstructor(particleParamItemClass, new Group<>(
+                            new Class<?>[]{particleClass, nmsItemStackClass},
+                            new Object[]{particle, ReflectionUtils.getStaticMethod("asNMSCopy", craftItemStackClass, new Group<>(new Class<?>[]{ItemStack.class}, new Object[]{((ItemParam) param).item}))}
+                    ));
+                } else if(param instanceof BlockDataParam){
+                    Object blockData = ReflectionUtils.getMethod("getState", craftBlockDataClass,ReflectionUtils.cast(craftBlockDataClass, ((BlockDataParam) param).data));
+                    particleParam = ReflectionUtils.getConstructor(particleParamBlockClass, new Group<>(
+                            new Class<?>[]{particleClass, IBlockData},
+                            new Object[]{particle, blockData}
+                    ));
+                } else if(param instanceof DustParam){
+                    DustParam dust = (DustParam) param;
                     particleParam = ReflectionUtils.getConstructor(particleParamRedstoneClass, new Group<>(
-                            new Class<?>[]{float.class, float.class, float.class, float.size},
-                            new Object[]{dust.getColor().getRed(), dust.getColor().getGreen(), dust.getColor().getBlue(), dust.size}
+                            new Class<?>[]{particleClass, float.class, float.class, float.class, float.class},
+                            new Object[]{particle, dust.getColor().getRed(), dust.getColor().getGreen(),
+                                    dust.getColor().getBlue(), dust.size}
                     ));
                 }
                 return new PacketSender(ReflectionUtils.getConstructor(packetPlayOutWorldParticlesClass, new Group<>(
@@ -193,17 +250,15 @@ public class Particle {
                 )));
             } else {
                     Class<?> enumParticleClass = Class.forName("net.minecraft.server."+v+".EnumParticle");
-                    Object enumParticle = ReflectionUtils.getEnum(type.toString(), enumParticleClass);
+                    Object enumParticle = ReflectionUtils.getEnum(param.particle.toString(), enumParticleClass);
                     int[] i = new int[]{};
-                    if(type.getDataClass().equals(Material.class)){
-                        i = new int[]{((Material) data).getId()};
-                    } else if(type.getDataClass().equals(ItemStack.class)){
-                        i = new int[]{((ItemStack) data).getType().getId()};
-                    } else if(type.getDataClass().equals(Dust.class)){
-                        Color color = ((Dust) data).color;
-                        offsetX = (float) color.getRed() / 255;
-                        offsetY = (float) color.getGreen() / 255;
-                        offsetZ = (float) color.getBlue() / 255;
+                    if(param instanceof ItemParam){
+                        i = new int[]{((ItemParam) param).item.getType().getId()};
+                    } else if(param instanceof DustParam){
+                        DustParam dust = (DustParam) param;
+                        offsetX = (float) dust.color.getRed() / 255;
+                        offsetY = (float) dust.color.getGreen() / 255;
+                        offsetZ = (float) dust.color.getBlue() / 255;
                         if (offsetX < 0) {
                             offsetX = 0;
                         }
@@ -219,10 +274,10 @@ public class Particle {
                             new Object[]{enumParticle, longDistance,
                                     (float) location.getX(), (float) location.getY(), (float) location.getZ(), offsetX, offsetY, offsetZ, speed, count, i}
                     )));
-                return null;
             }
         } catch(ClassNotFoundException e) {
             e.printStackTrace();
         }
+        return null;
     }
 }
