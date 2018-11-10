ef1f582997024fe2f33a0d67e9f35263bb9d55d0
Anh Craft <huynhduyanh123123@gmail.com>
2018-11-11 06:58:53 +0700

    Improved CompatibilityUtils

--- SpaciousLibSpigot/src/main/java/org/anhcraft/spaciouslib/utils/CompatibilityUtils.java
+++ SpaciousLibSpigot/src/main/java/org/anhcraft/spaciouslib/utils/CompatibilityUtils.java
@@ -13,11 +13,15 @@ public class CompatibilityUtils {
      * @return the inventory of that event
      */
     public static Inventory getInventory(InventoryClickEvent event){
-        Class<?> c = event.getClass().getSuperclass().getSuperclass();
-        if(event.getClass().equals(InventoryCreativeEvent.class)
-                || event.getClass().equals(CraftItemEvent.class)) {
-            c = c.getSuperclass();
+        if(GameVersion.is1_9Above()) {
+            return event.getClickedInventory();
+        } else {
+            Class<?> c = event.getClass().getSuperclass().getSuperclass();
+            if(event.getClass().equals(InventoryCreativeEvent.class)
+                    || event.getClass().equals(CraftItemEvent.class)) {
+                c = c.getSuperclass();
+            }
+            return (Inventory) ReflectionUtils.getMethod("getInventory", c, event);
         }
-        return (Inventory) ReflectionUtils.getMethod("getInventory", c, event);
     }
 }
