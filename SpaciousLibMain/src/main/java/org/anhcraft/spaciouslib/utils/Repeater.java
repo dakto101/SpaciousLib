bd9390a8a54185e827e6596636a3eb19c62fa85f
Anh Craft <huynhduyanh123123@gmail.com>
2018-12-30 21:13:20 +0700

    Repeater returns result

--- SpaciousLibMain/src/main/java/org/anhcraft/spaciouslib/utils/Repeater.java
+++ SpaciousLibMain/src/main/java/org/anhcraft/spaciouslib/utils/Repeater.java
@@ -1,42 +1,53 @@
 package org.anhcraft.spaciouslib.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Represents a repeating operation. The repeater is same as loop statement but has side-effects.
+ * The repeater will return the result as a list of object
  */
-public abstract class Repeater {
+public abstract class Repeater<T> {
     /**
      * Repeat an operation while the condition is true
      * @param init the initiation count
      * @param step the step count
      * @param repeater the repeater
+     * @return a list of object
      */
-    public static void whileTrue(int init, int step, Repeater repeater){
+    public static <T> List<T> whileTrue(int init, int step, Repeater<T> repeater){
+        List<T> list = new ArrayList<>();
         int i = init;
         while(repeater.check(i)){
-            repeater.run(i);
+            list.add(repeater.run(i));
             i += step;
         }
+        return list;
     }
 
     /**
-     * Repeat an operation until the condition is true
+     * Repeat an operation and stop when the condition is true.
      * @param init the initiation count
      * @param step the step count
      * @param repeater the repeater
+     * @return a list of object
      */
-    public static void until(int init, int step, Repeater repeater){
+    public static <T> List<T> until(int init, int step, Repeater<T> repeater){
+        List<T> list = new ArrayList<>();
         int i = init;
-        while(!repeater.check(i)){
-            repeater.run(i);
+        do {
+            list.add(repeater.run(i));
             i += step;
-        }
+        } while(!repeater.check(i));
+        return list;
     }
 
     /**
      * Execute the operation by calling this method
      * @param current the current count
+     * @return an object
      */
-    public abstract void run(int current);
+    public abstract T run(int current);
 
     /**
      * Check the given condition
